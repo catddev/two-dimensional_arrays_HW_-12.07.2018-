41b15382dcacd1ec5721ddbb9be14b8a6732ee6e
dinara0708 <aikenova01@gmail.com>
2018-07-14 19:35:26 +0600

    -

--- two-dimensional_arrays_HW_(12.07.2018)/Source.cpp
+++ two-dimensional_arrays_HW_(12.07.2018)/Source.cpp
@@ -1,1 +1,44 @@
-﻿
+﻿#include <iostream>
+#include <iomanip>
+#include<math.h>
+#include<ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(time(NULL));
+
+	setlocale(LC_ALL, "Rus");
+
+	int tn = 0;
+
+while (true)
+{
+	cout << "Задача № ";
+	cin >> tn;
+
+	switch (tn)
+	{
+	case 1:
+	{
+
+	}
+	break;
+	case 2:
+	{
+
+	}
+	break;
+	case 3:
+	{
+
+	}
+	break;
+	default:
+		cout << "нет такой задачи" << endl << endl;
+	}
+}
+	system("pause");
+	return 0;
+}
